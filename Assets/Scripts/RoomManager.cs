706f1c38de329dad3faa0a82e56eef0f2e51e7e8
PePo <gmgame.1525@gmail.com>
2019-04-13 16:49:24 +0700

    Waiting SceneLayout

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -3,63 +3,40 @@ using Firebase.Database;
 using Firebase.Unity.Editor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class RoomManager : MonoBehaviour
 {
     #region Variables
-    [Header("Create UI")]
-    [SerializeField] private TMP_InputField CreateUI_PlayerNameText;
-
+    [Header("Waiting UI")]
+    [SerializeField] private TextMeshProUGUI WaitingUI_RoomTokenText;
+    [SerializeField] PlayerData[] players;
     [Space]
 
-    [Header("Join UI")]
+    [Header("Mode Select UI")]
     [SerializeField] private TMP_InputField JoinUI_PlayerNameText;
     [SerializeField] private TMP_InputField JoinUI_RoomNameText;
     #endregion
 
+    public struct PlayerData
+    {
+        private Image ProfilePicture;
+        private TextMeshProUGUI Name;
+    }
+
     #region Core Method
     private void Awake()
     {
+        WaitingUI_RoomTokenText.SetText(PlayerPrefs.GetString("RoomToken"));
+
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(Config.FirebaseURL);
     }
     #endregion
 
     #region Utils Method
-    public void CreateRoom()
+    public void Enter()
     {
-        DatabaseReference databaseReference = FirebaseDatabase.DefaultInstance.RootReference;
-
-        var roomToken = Utils.GetRandomToken();
-        PlayerPrefs.SetString("RoomToken", roomToken);
-
-        DatabaseReference roomReference = databaseReference.Child(roomToken);
-        roomReference.Child("Player1").SetValueAsync(CreateUI_PlayerNameText.text).ContinueWith(task =>
-        {
-            if (task.IsCompleted)
-            {
-                roomReference.Child("Status").SetValueAsync("Waiting").ContinueWith(task2 =>
-                {
-                    if (task2.IsCompleted)
-                    {
-                        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-                    }
-                });
-            }
-        });
-
-    }
-
-    public void JoinRoom()
-    {
-        DatabaseReference databaseReference = FirebaseDatabase.DefaultInstance.GetReference(JoinUI_RoomNameText.text);
-        databaseReference.Child("Status").GetValueAsync().ContinueWith(task =>
-        {
-            if (task.IsCompleted)
-            {
-                var snapshot = task.Result;
-            }
-        });
 
     }
     #endregion
