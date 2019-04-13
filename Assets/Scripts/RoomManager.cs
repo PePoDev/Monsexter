915be65a79b21e48a754345d75914c9327af6080
PePo <gmgame.1525@gmail.com>
2019-04-14 01:36:48 +0700

    Add OnApplicationQuit to remove User from Room

--- Assets/Scripts/RoomManager.cs
+++ Assets/Scripts/RoomManager.cs
@@ -1,11 +1,12 @@
 ﻿using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
-using TMPro;
 using System;
+using System.Collections;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RoomManager : MonoBehaviour
 {
@@ -30,15 +31,89 @@ public class RoomManager : MonoBehaviour
     #region Core Method
     private void Awake()
     {
-        WaitingUI_RoomTokenText.SetText(PlayerPrefs.GetString("RoomToken"));
-
         FirebaseApp.DefaultInstance.SetEditorDatabaseUrl(Config.FirebaseURL);
-        var roomReference = FirebaseDatabase.DefaultInstance.GetReference(PlayerPrefs.GetString("RoomToken"));
+        WaitingUI_RoomTokenText.SetText(PlayerPrefs.GetString("RoomToken"));
+        StartCoroutine(Initialization());
+    }
 
+    private void OnApplicationQuit()
+    {
+        var refDB = FirebaseDatabase.DefaultInstance.GetReference(PlayerPrefs.GetString("RoomToken"));
+        refDB.Child($"Player{PlayerPrefs.GetInt("PlayerIndex").ToString()}").RemoveValueAsync();
+    }
+
+    IEnumerator Initialization()
+    {
+        DatabaseReference roomReference = FirebaseDatabase.DefaultInstance.GetReference(PlayerPrefs.GetString("RoomToken"));
+
+        var completeDataNumber = 0;
+        var playersName = new string[6];
+        for (var i = 1; i <= Config.MaxPlayer; i++)
+        {
+            var x = i;
+            roomReference.Child($"Player{x}").GetValueAsync().ContinueWith(playerTask =>
+            {
+                if (playerTask.IsCompleted && playerTask.Result.Exists)
+                {
+                    playersName[x - 1] = playerTask.Result.Value as string;
+                }
+                completeDataNumber++;
+            });
+        }
+
+        yield return new WaitUntil(()=> completeDataNumber == 6);
+
+        for (var i = 0; i < Config.MaxPlayer; i++)
+        {
+            players[i].Name.SetText(playersName[i]);
+        }
+
+        roomReference.ChildAdded += HandleChildAdded;
+        roomReference.ChildChanged += HandleChildChanged;
+        roomReference.ChildRemoved += HandleChildRemoved;
+        roomReference.ChildMoved += HandleChildMoved;
+    }
+
+    private void HandleChildAdded(object sender, ChildChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+    }
+
+    private void HandleChildChanged(object sender, ChildChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+    }
+
+    private void HandleChildRemoved(object sender, ChildChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
+    }
+
+    private void HandleChildMoved(object sender, ChildChangedEventArgs args)
+    {
+        if (args.DatabaseError != null)
+        {
+            Debug.LogError(args.DatabaseError.Message);
+            return;
+        }
     }
     #endregion
 
     #region Utils Method
+
+
     public void Enter()
     {
 
