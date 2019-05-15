468eee37c0e32f7b70ad4318fca1cacfe3e9464a
PePoDev <gmgame.1525@gmail.com>
2019-05-15 16:05:44 +0700

    Change Firebase Database to Customer Account

--- Assets/Scripts/Config.cs
+++ Assets/Scripts/Config.cs
@@ -3,7 +3,7 @@ using System;
 
 public sealed class Config : SCSingletonMB<Config>
 {
-    public const string FirebaseURL = "https://monsexter.firebaseio.com/";
+    public const string FirebaseURL = "https://monsexter-3b9e3.firebaseio.com/";
     public const int MaxPlayer = 6;
     public const int TimeCapture = 6;
     public readonly long TrickFromTimeCapture = TimeSpan.FromMinutes(6).Ticks;
